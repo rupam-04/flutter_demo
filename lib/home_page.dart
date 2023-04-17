963ab6ca15dc22fa0f159d7875b8087b45d6f703
rupam-04 <rupam.dey.it26@heritageit.edu.in>
2023-04-18 00:02:41 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  final int days = 30;
+  final String name = "Codepur";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Catalog App"),
+        ),
+        body: Center(
+          child: Text('Welcome to $days days of flutter by $name'),
+        ),
+        drawer: Drawer(),
+      );
+  }
+}
