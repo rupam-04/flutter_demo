14f554997868663eddcf188be0cce315f5ccdd6c
rupam-04 <rpmdey2004@gmail.com>
2023-04-23 01:32:10 +0530

    Day 8 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -13,82 +13,99 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String name = "";
   bool changeButton = false;
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async { // async is reqd for await
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        changeButton = true;
+      });
+      await Future.delayed(Duration(seconds: 1)); //wait 1 sec
+      await Navigator.pushNamed(context, MyRoutes.homeRoute); //if you press back after this it goes back to login page which is basically pop()
+      setState(() {
+        changeButton = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset("assets/images/login_image.png", fit: BoxFit.cover,),
-            SizedBox(height: 20,), // for spacing
-            Text("Welcome $name", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
-            SizedBox(height: 20,),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-              child: Column(children: [
-                TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Enter Username",
-                  labelText: "Username",
-                ),
-                onChanged: (value) {
-                  name = value;
-                  setState(() {}); //this calls the build function
-                },
-              ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.asset("assets/images/login_image.png", fit: BoxFit.cover,),
+              SizedBox(height: 20,), // for spacing
+              Text("Welcome $name", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
               SizedBox(height: 20,),
-              TextFormField(
-                obscureText: true, // hides the input password
-                decoration: InputDecoration(
-                  hintText: "Enter Password",
-                  labelText: "Password",
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+                child: Column(children: [
+                  TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "Username",
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Username cannot be empty";
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    name = value;
+                    setState(() {}); //this calls the build function
+                  },
                 ),
+                SizedBox(height: 20,),
+                TextFormField(
+                  obscureText: true, // hides the input password
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Password cannot be empty";
+                    }
+                    else if (value.length<6) {
+                      return "Password length should be atleast 6";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 40,),
+        
+                Material(
+                  color: Colors.deepPurple, //always give decoration to ancestor and not child
+                  borderRadius: BorderRadius.circular(changeButton? 50: 8),
+                  child: InkWell(
+                    onTap: () => moveToHome(context),
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: changeButton? 50: 150, //ternary
+                      height: 50, 
+                      // color: Colors.deepPurple,
+                      alignment: Alignment.center,
+                      child: changeButton? Icon(Icons.done, color: Colors.white,):
+                      Text(
+                        "Login",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ), // same as container but provides more decorations
+                  ),
+                ) // can transform container to a button
+                ],),
               ),
-              SizedBox(height: 40,),
-
-              InkWell(
-                onTap: () async {
-                  setState(() {
-                    changeButton = true;
-                  });
-                  await Future.delayed(Duration(seconds: 1)); //wait 1 sec
-                  Navigator.pushNamed(context, MyRoutes.homeRoute); //if you press back after this it goes back to login page which is basically pop()
-                },
-                child: AnimatedContainer(
-                  duration: Duration(seconds: 1),
-                  width: changeButton? 50: 150, //ternary
-                  height: 50, 
-                  // color: Colors.deepPurple,
-                  alignment: Alignment.center,
-                  child: changeButton? Icon(Icons.done, color: Colors.white,):
-                  Text(
-                    "Login",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.deepPurple, 
-                    // shape: changeButton? BoxShape.circle: BoxShape.rectangle,
-                    borderRadius: BorderRadius.circular(changeButton? 50: 8),
-                  ),
-                ),
-              ) // can transform container to a button
-
-              // ElevatedButton(
-              //   child: Text("Login"),
-              //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-              //   onPressed: () {
-              //     Navigator.pushNamed(context, MyRoutes.homeRoute); //if you press back after this it goes back to login page which is basically pop()
-              //   },
-              //   )
-              ],),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
