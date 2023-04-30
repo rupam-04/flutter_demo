9c391cf3358e034c2e75d66633d5254488ae8bb3
rupam-04 <rpmdey2004@gmail.com>
2023-05-01 01:42:44 +0530

    Day 10 completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_demo/utils/routes.dart';
 import 'package:flutter_demo/widgets/themes.dart';
 import 'pages/home_page.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'pages/login_page.dart';
 
 void main() {
