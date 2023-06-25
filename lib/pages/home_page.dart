28660fda7d62d0da706cec5b11921f232049664b
rupam-04 <rpmdey2004@gmail.com>
2023-06-26 00:34:54 +0530

    day 12 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/models/catalog.dart';
 
 import '../widgets/drawer.dart';
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -11,12 +13,21 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
         appBar: AppBar(
           title: const Text("Catalog App",)
         ),
-        body: Center(
-          child: Text(context.runtimeType.toString()),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.builder(
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: dummyList[index], key: Key(index.toString()),
+              );
+            } 
+          ),
         ),
         drawer: const MyDrawer(),
       );
---
+++ lib/widgets/item_widget.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: unnecessary_null_comparison
+
+import 'package:flutter/material.dart';
+
+import '../models/catalog.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({required Key key, required this.item}): assert(item != null), super(key: key); 
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: () {
+          print("${item.name} pressed");
+        },
+        leading: Image.network(item.image),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text(
+          "\$${item.price.toString()}",
+          textScaleFactor: 1.5,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
