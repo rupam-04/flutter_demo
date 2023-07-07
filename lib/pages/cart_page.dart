949e5e72d130cb00362805868786a1a969fdb3c4
rupam-04 <rpmdey2004@gmail.com>
2023-07-07 23:09:17 +0530

    day 21 completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -12,6 +13,64 @@ class CartPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: "Cart".text.make(),
+      ),
+
+      body: Column(
+        children: [
+          _CartList().p32().expand(),
+          Divider(),
+          _CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          "\$9999".text.xl3.bold.color(Colors.black).make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: () {}, 
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(MyTheme.darkBluishColor),
+              shape: MaterialStateProperty.all(StadiumBorder())
+            ),
+            child: "Buy".text.color(Colors.white).make().px4().py2()
+          ).w24(context)
+        ],
+      ).px16()
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  const _CartList({super.key});
+
+  @override
+  State<_CartList> createState() => __CartListState();
+}
+
+class __CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(CupertinoIcons.check_mark_circled),
+        trailing: IconButton(
+          onPressed: () {}, 
+          icon: Icon(CupertinoIcons.delete)
+        ),
+        title: "Item 1".text.make(),
       ),
     );
   }
